ea1b34da82ac33ef19ccc23e3e99d4859e5b87d9
thecrazyskull <anaskarbila@aospa.co>
2018-05-15 11:58:02 -0400

    policy: Add support for device alert sliders
    * Provide support for alert slider hardware.
    * Ability to swap order of alert slider within system setting.
    * Enable setting config_hasAlertSlider to true and defining
      proc nodes for state path and UEvent match path.
    
    * Contributors:
    @thecrazyskull
    @carlosavignano
    
    TODO> SystemUI integration.
    
    Forward ported to Pie by @Sirhc95
    
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>
    Signed-off-by: rounak1619 <rounakmehata9@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5022,6 +5022,23 @@ public final class Settings {
          */
         public static final String DOZE_TRIGGER_DOUBLETAP = "doze_trigger_doubletap";
 
+	/**
+         * Whether user can swap the order of the Alert Slider.
+         * * Whether user can invert the order of the Alert Slider.
+         * 0: Default
+         * 1: Inverted
+         * @hide
+         */
+        public static final String ALERT_SLIDER_ORDER = "alert_slider_order";
+
+        /**
+         * Preferred silent mode for Alert Slider..
+         * 0: Alarms only.
+         * 1: Total silence
+         * @hide
+         */
+        public static final String ALERT_SLIDER_SILENT_MODE = "alert_slider_silent_mode";
+
         /**
          * These are all public system settings
          *
