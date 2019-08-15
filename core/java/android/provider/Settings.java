aab1e119954f84b7c68e204dee491ac8d71017ff
Sam Mortimer <sam@mortimer.me.uk>
2019-08-15 14:48:00 -0700

    Add secure setting to allow tethering via VPN upstream
    Change-Id: I4665be19f425c47766f29da5993fd18b19352b4b

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -10272,6 +10272,12 @@ public final class Settings {
         public static final String GAME_DASHBOARD_ALWAYS_ON = "game_dashboard_always_on";
 
         /**
+         * Whether tethering is allowed to use VPN upstreams
+         */
+        @SuppressLint("NoSettingsProvider")
+        public static final String TETHERING_ALLOW_VPN_UPSTREAMS = "tethering_allow_vpn_upstreams";
+
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
