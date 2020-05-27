390e657948dc0e4c6f1e001b461bc41a56842e6a
Ashwin R C <ashwin2001achu@gmail.com>
2020-05-27 10:26:16 +0000

    SystemUI: Adapt screenshot sound to ringer modes
    Change-Id: I381c351131241e45ddb6049706d6c302c2eee946

--- packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
@@ -50,6 +50,7 @@ import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.InsetDrawable;
 import android.graphics.drawable.LayerDrawable;
+import android.media.AudioManager;
 import android.media.MediaActionSound;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
@@ -58,6 +59,9 @@ import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
 import android.os.RemoteException;
+import android.os.UserHandle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.provider.Settings;
 import android.util.DisplayMetrics;
 import android.util.Log;
@@ -87,6 +91,7 @@ import com.android.internal.logging.UiEventLogger;
 import com.android.systemui.R;
 import com.android.systemui.dagger.qualifiers.Main;
 import com.android.systemui.dagger.qualifiers.UiBackground;
+import com.android.systemui.shared.system.ActivityManagerWrapper;
 import com.android.systemui.shared.system.QuickStepContract;
 import com.android.systemui.shared.system.TaskStackChangeListener;
 
@@ -217,6 +222,8 @@ public class GlobalScreenshot implements ViewTreeObserver.OnComputeInternalInset
     private float mDismissDeltaY;
 
     private Ringtone mScreenshotSound;
+    private AudioManager mAudioManager;
+    private Vibrator mVibrator;
 
     private int mNavMode;
     private int mLeftInset;
@@ -325,6 +332,10 @@ public class GlobalScreenshot implements ViewTreeObserver.OnComputeInternalInset
 
         // Initialize current foreground package name
         mTaskListener.onTaskStackChanged();
+
+        // Grab system services needed for screenshot sound
+        mAudioManager = (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
+        mVibrator = (Vibrator) mContext.getSystemService(Context.VIBRATOR_SERVICE);
     }
 
     @Override // ViewTreeObserver.OnComputeInternalInsetsListener
@@ -722,12 +733,31 @@ public class GlobalScreenshot implements ViewTreeObserver.OnComputeInternalInset
                     }
                 });
 
-                // Play the shutter sound to notify that we've taken a screenshot
-                if (Settings.System.getIntForUser(mContext.getContentResolver(), Settings.System.SCREENSHOT_SOUND, 1, UserHandle.USER_CURRENT) == 1) {
-                    if (mScreenshotSound != null) {
-                        mScreenshotSound.play();
-                    }
-                }
+                mScreenshotLayout.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        switch (mAudioManager.getRingerMode()) {
+                            case AudioManager.RINGER_MODE_SILENT:
+                                // do nothing
+                                break;
+                            case AudioManager.RINGER_MODE_VIBRATE:
+                                if (mVibrator != null && mVibrator.hasVibrator()) {
+                                    mVibrator.vibrate(VibrationEffect.createOneShot(50,
+                                            VibrationEffect.DEFAULT_AMPLITUDE));
+                                }
+                                break;
+                            case AudioManager.RINGER_MODE_NORMAL:
+                                // Play the shutter sound to notify that we've taken a screenshot
+                                if (Settings.System.getInt(mContext.getContentResolver(),
+                                        Settings.System.SCREENSHOT_SOUND, 1) == 1) {
+                                    if (mScreenshotSound != null) {
+                                        mScreenshotSound.play();
+                                    }
+                                }
+                                break;
+                            }
+                        }
+                });
 
                 mScreenshotPreview.setLayerType(View.LAYER_TYPE_HARDWARE, null);
                 mScreenshotPreview.buildLayer();
