40ea063adf6c330c85a217e6c77576501675190b
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-09-12 12:39:49 +0530

    overlays: Add Google Sans font
    Change-Id: Ia181213a81c08fbd88991ce448262ab33a448148

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -39,6 +39,7 @@ LOCAL_REQUIRED_MODULES := \
 	FontArvoLatoOverlay \
         FontArbutusSourceOverlay \
 	FontRubikRubikOverlay \
+	FontGoogleSansOverlay \
 	IconPackCircularAndroidOverlay \
 	IconPackCircularLauncherOverlay \
 	IconPackCircularSettingsOverlay \
