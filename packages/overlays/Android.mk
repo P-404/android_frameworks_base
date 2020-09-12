1bd47e621422b839972c5561f707c27cd6475983
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-09-12 12:29:42 +0530

    overlays: Add Arbutus Slab / Source Sans Pro font
    Change-Id: I7c6c5c0856b56d3f38c39e901724cbe2d285bc9b

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -37,6 +37,7 @@ LOCAL_REQUIRED_MODULES := \
 	DisplayCutoutEmulationWaterfallOverlay \
 	FontNotoSerifSourceOverlay \
 	FontArvoLatoOverlay \
+        FontArbutusSourceOverlay \
 	IconPackCircularAndroidOverlay \
 	IconPackCircularLauncherOverlay \
 	IconPackCircularSettingsOverlay \
