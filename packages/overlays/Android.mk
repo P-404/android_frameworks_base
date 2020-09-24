40ab7f3a6095d7e444f797bb0162b2d89fdccd42
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-09-24 15:18:48 +0530

    overlays: Add iOS Blue accent
     * based on https://github.com/ProtonAOSP/android_frameworks_base/commit/acb62476cc39167d1134b46c930df666fc167170
    
    Change-Id: I2ad087fb2684941a8a5effcf86060d8f710ac66f
    Signed-off-by: Akash <akashniki@gmail.com>

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -30,6 +30,7 @@ LOCAL_REQUIRED_MODULES := \
 	AccentColorAmethystOverlay \
 	AccentColorAquamarineOverlay \
 	AccentColorTangerineOverlay \
+        AccentColoriOSBlueOverlay \
 	FontNotoSerifSourceOverlay \
 	FontArvoLatoOverlay \
         FontArbutusSourceOverlay \
