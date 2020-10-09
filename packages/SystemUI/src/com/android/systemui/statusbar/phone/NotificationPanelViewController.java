47b019a23de7a13961da6cb987c367991e1cd171
00day0 <therandomuser11@gmail.com>
2020-10-09 23:34:40 +0200

    SystemUI: Introduce OOS style notification clear all button
    cleaned up by panda cleanup service.
    
    Signed-off-by: 00day0 <therandomuser11@gmail.com>
    Co-Authored-by: Jyotiraditya Panda <jyotiraditya@aospa.co>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
@@ -1276,7 +1276,7 @@ public class NotificationPanelViewController extends PanelViewController {
         return !mQsTouchAboveFalsingThreshold;
     }
 
-    private float getQsExpansionFraction() {
+    public float getQsExpansionFraction() {
         return Math.min(
                 1f, (mQsExpansionHeight - mQsMinExpansionHeight) / (mQsMaxExpansionHeight
                         - mQsMinExpansionHeight));
@@ -2182,6 +2182,7 @@ public class NotificationPanelViewController extends PanelViewController {
             alpha *= mClockPositionResult.clockAlpha;
         }
         mNotificationStackScroller.setAlpha(alpha);
+        mStatusBar.updateDismissAllVisibility(true);
     }
 
     private float getFadeoutAlpha() {
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PanelViewController.java
@@ -1072,6 +1072,7 @@ public abstract class PanelViewController {
 
     public void setLaunchingNotification(boolean launchingNotification) {
         mLaunchingNotification = launchingNotification;
+        mStatusBar.updateDismissAllVisibility(false);
     }
 
     public void collapseWithDuration(int animationDuration) {
