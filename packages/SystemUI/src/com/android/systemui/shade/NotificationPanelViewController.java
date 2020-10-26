1e8133e49688ce45ecece42ed1b443ae2acdf3da
Anas Karbila <anaskarbila@aospa.co>
2020-10-26 10:27:40 -0300

    SystemUI: Double tap to sleep on lockscreen
    Change-Id: Icd8de7c5512362f22dcea9c3946347a1f0c13aba

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -48,6 +48,7 @@ import android.annotation.NonNull;
 import android.app.Fragment;
 import android.app.StatusBarManager;
 import android.content.ContentResolver;
+import android.content.Context;
 import android.database.ContentObserver;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -73,6 +74,7 @@ import android.transition.TransitionManager;
 import android.util.IndentingPrintWriter;
 import android.util.Log;
 import android.util.MathUtils;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.VelocityTracker;
@@ -532,6 +534,7 @@ public final class NotificationPanelViewController extends PanelViewController {
 
     private final NotificationShadeDepthController mDepthController;
     private final int mDisplayId;
+    private GestureDetector mDoubleTapGestureListener;
 
     private KeyguardIndicationController mKeyguardIndicationController;
     private int mHeadsUpInset;
@@ -877,6 +880,16 @@ public final class NotificationPanelViewController extends PanelViewController {
         });
         mBottomAreaShadeAlphaAnimator.setDuration(160);
         mBottomAreaShadeAlphaAnimator.setInterpolator(Interpolators.ALPHA_OUT);
+        mDoubleTapGestureListener = new GestureDetector(mView.getContext(),
+                new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public boolean onDoubleTap(MotionEvent event) {
+                final PowerManager pm = (PowerManager) mView.getContext().getSystemService(
+                        Context.POWER_SERVICE);
+                pm.goToSleep(event.getEventTime());
+                return true;
+            }
+        });
         mConversationNotificationManager = conversationNotificationManager;
         mAuthController = authController;
         mLockIconViewController = lockIconViewController;
@@ -4251,6 +4264,10 @@ public final class NotificationPanelViewController extends PanelViewController {
                 if (mCentralSurfaces.isBouncerShowingScrimmed()
                         || mCentralSurfaces.isBouncerShowingOverDream()) {
                     return false;
+                }
+
+                if (mBarState == StatusBarState.KEYGUARD) {
+                    mDoubleTapGestureListener.onTouchEvent(event);
                 }
 
                 // Make sure the next touch won't the blocked after the current ends.
