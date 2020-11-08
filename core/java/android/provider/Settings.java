caf2fc7b74c79f210c76d7cf0ffb1513f02b3881
ezio84 <brabus84@gmail.com>
2020-11-08 10:17:01 +0100

    Ambient music: pulse on new music tracks and Now Playing info [1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4821,6 +4821,12 @@ public final class Settings {
          */
         public static final String LOCKSCREEN_MEDIA_ART = "lockscreen_media_art";
 
+        /** Whether to pulse ambient on new music tracks
+         *
+         * @hide
+         */
+        public static final String PULSE_ON_NEW_TRACKS = "pulse_on_new_tracks";
+
         /**
          * Whether to enable PULSE Edge lights
          * @hide
