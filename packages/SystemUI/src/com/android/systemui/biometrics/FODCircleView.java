360730db2a868e2cc0d1d7adc337d4e68f37d6e0
AdarshGrewal <adarshgrewal@gmail.com>
2020-11-27 18:28:53 +0300

    FODCircleView: Hide fod when screen is off
    It seems that onHideFODView() will not be called when
    the screen goes off by timeout under ambient display.
    
    Obviously, this case cannot be handled by checking
    doze status. Move to onScreenTurnedOff instead.
    
    Test: reproduce this issue on OnePlus 7 Pro
          and see if notify_dim is set to 0
    
    Signed-off-by: alk3pInjection <webmaster@raspii.tech>
    Change-Id: Ib47f90eab806ffa7ad67b650fce3a4382b9d82e4

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -168,7 +168,7 @@ public class FODCircleView extends ImageView {
         }
 
         @Override
-        public void onStartedGoingToSleep(int why) {
+        public void onScreenTurnedOff() {
             hide();
         }
 
