30175b12ebf2d06c2a2623bccf2249326afbdb68
Han Wang <416810799@qq.com>
2021-02-17 06:17:04 +0100

    base: Install seccomp filter even if selinux is permissive
     * If selinux is permissive, Google decide to disable seccomp
       filter because debug tools like strace needs both them to be
       disabled to work.
    
     * This is fine for developers, but if one runs selinux permissive
       in production, it will be super easy for a macilious app to gain
       root access on Q+, with newly interoduced ZygotePreload API.
    
     * Technical details:
       https://github.com/dantmnf/BrokenSandbox
       https://github.com/vvb2060/Magica
    
    Change-Id: I1a1208a0a82d36aeb49820972429d106919d6bce
    (cherry picked from commit 32cf8c9cb1915ef67526c4e961ecb6a2181b6934)

--- core/jni/com_android_internal_os_Zygote.cpp
+++ core/jni/com_android_internal_os_Zygote.cpp
@@ -129,8 +129,6 @@ static constexpr const char* kZygoteInitClassName = "com/android/internal/os/Zyg
 static jclass gZygoteInitClass;
 static jmethodID gGetOrCreateSystemServerClassLoader;
 
-static bool gIsSecurityEnforced = true;
-
 /**
  * True if the app process is running in its mount namespace.
  */
@@ -653,11 +651,6 @@ static void PreApplicationInit() {
 }
 
 static void SetUpSeccompFilter(uid_t uid, bool is_child_zygote) {
-  if (!gIsSecurityEnforced) {
-    ALOGI("seccomp disabled by setenforce 0");
-    return;
-  }
-
   // Apply system or app filter based on uid.
   if (uid >= AID_APP_START) {
     if (is_child_zygote) {
@@ -2349,11 +2342,6 @@ static void com_android_internal_os_Zygote_nativeAllowFileAcrossFork(
 
 static void com_android_internal_os_Zygote_nativeInstallSeccompUidGidFilter(
         JNIEnv* env, jclass, jint uidGidMin, jint uidGidMax) {
-  if (!gIsSecurityEnforced) {
-    ALOGI("seccomp disabled by setenforce 0");
-    return;
-  }
-
   bool installed = install_setuidgid_seccomp_filter(uidGidMin, uidGidMax);
   if (!installed) {
       RuntimeAbort(env, __LINE__, "Could not install setuid/setgid seccomp filter.");
@@ -2429,10 +2417,6 @@ static void com_android_internal_os_Zygote_nativeInitNativeState(JNIEnv* env, jc
   /*
    * Security Initialization
    */
-
-  // security_getenforce is not allowed on app process. Initialize and cache
-  // the value before zygote forks.
-  gIsSecurityEnforced = security_getenforce();
 
   selinux_android_seapp_context_init();
 
