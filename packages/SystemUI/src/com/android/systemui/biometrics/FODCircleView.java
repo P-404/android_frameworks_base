c0bf7b71fba35b8d3950e25dd7c0f2b059859c13
Vishalcj17 <vishalcj@aospa.co>
2021-06-19 01:39:11 +0900

    FODCircleView: Add Boost framework hook.
     * In my testing on OnePlus9 Pro, this had a drastic effect.
       Speeds went from 0.87s to 0.38s (average from 10 trials)
    
     * 2 second headroom is enough as the PerfLock is triggered
       onPress and released onHide.
    
    Change-Id: I8566ef1ee095b44f4f1d42016b9cdef16740e20f

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -32,6 +32,7 @@ import android.os.Handler;
 import android.os.Looper;
 import android.os.RemoteException;
 import android.provider.Settings;
+import android.util.BoostFramework;
 import android.view.Display;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -83,6 +84,10 @@ public class FODCircleView extends ImageView {
     private boolean mTouchedOutside;
     private boolean mCanUnlockWithFp;
     private boolean mFpDisabled;
+
+    private BoostFramework mPerfBoost;
+    private boolean mIsPerfLockAcquired = false;
+    private static final int BOOST_DURATION_TIMEOUT = 2000;
 
     private Handler mHandler;
 
@@ -332,6 +337,7 @@ public class FODCircleView extends ImageView {
 
         mUpdateMonitor = Dependency.get(KeyguardUpdateMonitor.class);
         mUpdateMonitor.registerCallback(mMonitorCallback);
+        mPerfBoost = new BoostFramework();
     }
 
     @Override
@@ -401,6 +407,12 @@ public class FODCircleView extends ImageView {
         } catch (RemoteException e) {
             // do nothing
         }
+        if (!mIsPerfLockAcquired) {
+            mPerfBoost.perfHint(BoostFramework.VENDOR_HINT_FIRST_LAUNCH_BOOST,
+                    null,
+                    BOOST_DURATION_TIMEOUT);
+            mIsPerfLockAcquired = true;
+        }
     }
 
     public void dispatchRelease() {
@@ -427,6 +439,10 @@ public class FODCircleView extends ImageView {
             daemon.onHideFODView();
         } catch (RemoteException e) {
             // do nothing
+        }
+        if (mIsPerfLockAcquired) {
+            mPerfBoost.perfLockRelease();
+            mIsPerfLockAcquired = false;
         }
     }
 
