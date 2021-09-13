4ae741ab65647da5c3fb66f03a14b94c08c69b21
Beverly <beverlyt@google.com>
2021-09-13 17:25:00 -0400

    Show UDFPS icon on AOD even if fp auth isn't running
    * shows udfps if it's enrolled on AOD even if:
       * device requires strong auth (ie: after reboot, lockdown)
       * device is unlocked via smart lock
    
    On click of the udfps icon in the above cases:
      * If the device is locked, the bouncer will show.
      * If the device is unlocked, the user will enter the device.
    
    Fixes: 198315404
    Test: manual
    Change-Id: Id766df8f77427ae1db672507716bd38dc3462452
    Merged-In: Id766df8f77427ae1db672507716bd38dc3462452
    (cherry picked from commit c06bb2d2a65aeb15ca194c9cb47702a94079dfec)
    Signed-off-by: Akash <akashniki@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
@@ -3680,6 +3680,7 @@ public class NotificationPanelViewController extends PanelViewController {
     }
 
     public void dozeTimeTick() {
+        mLockIconViewController.dozeTimeTick();
         mKeyguardBottomArea.dozeTimeTick();
         mKeyguardStatusViewController.dozeTimeTick();
         if (mInterpolatedDarkAmount > 0) {
@@ -3985,10 +3986,6 @@ public class NotificationPanelViewController extends PanelViewController {
                 if (event.getActionMasked() == MotionEvent.ACTION_DOWN && isFullyExpanded()
                         && mStatusBarKeyguardViewManager.isShowing()) {
                     mStatusBarKeyguardViewManager.updateKeyguardPosition(event.getX());
-                }
-
-                if (mLockIconViewController.onTouchEvent(event)) {
-                    return true;
                 }
 
                 handled |= super.onTouch(v, event);
