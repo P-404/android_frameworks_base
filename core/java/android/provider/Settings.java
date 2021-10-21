7ad454ec388d621b5790e460dddd67ebf028704d
ezio84 <brabus84@gmail.com>
2021-10-21 18:07:40 +0000

    base: Long press power button to toggle torch [1/2]
     * Thanks to beanstown106 for the initial longpress action calls in
       PhoneWindowManager (improved by lineage guys)
    
     * Remove the settings pref if the device doesn't have flashlight
    
     * Vibrate when toggling torch to let the user know of unwanted toggling
       in the pocket (even if vibration is disabled by user)
    
     * Allow torch action also on ambient display
    
    [AEX Edits] :
    - Port to S
    - simplify code
    
    [mydongistiny]:
    - Fixed for android10
    
    [DennySPB] :
    - base: ScreenOff torch: Allow to disable torch when screen is on
    
    [mickaelmendes50] :
    - Adapt to AOSPA
    
     * We don't allow users to use the Double Press power option,
       so remove the non used code.
     * Removed "double press power to toggle torch"
     * Removed "handle camera gesture here instead of extensions"
     * Removed "Add 170ms delay for double tap action"
    
    [ghostrider-reborn: updated for 13]
    
    Change-Id: I12da044f86c7b625872607529cf8524615cf576b
    Co-authored-by: DennySPb <dennyspb@gmail.com>
    Co-authored-by: Mesquita <mesquita@aospa.co>
    Signed-off-by: Adin Kwok <adin.kwok@carbonrom.org>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5554,6 +5554,15 @@ public final class Settings {
         public static final String SHOW_LOCKSCREEN_MEDIA_ART = "show_lockscreen_media_art";
 
         /**
+         * Whether the torch launch gesture to double tap or long press the power button when the
+         * screen is off should be enabled. *
+         * 0: disabled
+         * 1: long tap power for torch
+         * @hide
+         */
+        public static final String TORCH_POWER_BUTTON_GESTURE = "torch_power_button_gesture";
+        
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
