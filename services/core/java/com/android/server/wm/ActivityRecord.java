a28f1f809eb5ddf06ce4f9a55727b7fb7af2bf7d
Cheng Shi <shicheng@xiaomi.corp-partner.google.com>
2021-10-22 06:55:04 +0000

    fwb: [Bugfix] Fix the issue of transition animation splash screen in split screen mode.
    When an app enters split-screen mode, the mAnimatingActivityRegistry of the current Activity still comes from the app's Task, but the mAnimatingActivityRegistry of the newly opened Activity comes from the split-screen Task, so there is no guarantee that the animation-leash of the transition animation will be destroyed at the same time.
    So we ensure that the mAnimatingActivityRegistry of ActivityRecord under the same Task is the same after entering split screen mode, so as to ensure that the transition animation of activity opening and closing ends at the same time, thereby avoiding splash screen.
    
    Bug: 204024254
    Test: Above scenario
    Change-Id: Ibf0fccecefaa8768b4e10f69a064a4b7452dcaa9
    Signed-off-by: shicheng <shicheng@xiaomi.com>

--- services/core/java/com/android/server/wm/ActivityRecord.java
+++ services/core/java/com/android/server/wm/ActivityRecord.java
@@ -1467,13 +1467,7 @@ public final class ActivityRecord extends WindowToken implements WindowManagerSe
         }
         final Task rootTask = getRootTask();
 
-        // If we reparent, make sure to remove ourselves from the old animation registry.
-        if (mAnimatingActivityRegistry != null) {
-            mAnimatingActivityRegistry.notifyFinished(this);
-        }
-        mAnimatingActivityRegistry = rootTask != null
-                ? rootTask.getAnimatingActivityRegistry()
-                : null;
+        updateAnimatingActivityRegistry();
 
         if (task == mLastParentBeforePip) {
             // Activity's reparented back from pip, clear the links once established
@@ -1504,6 +1498,20 @@ public final class ActivityRecord extends WindowToken implements WindowManagerSe
                 rootTask.setHasBeenVisible(true);
             }
         }
+    }
+
+    void updateAnimatingActivityRegistry() {
+        final Task rootTask = getRootTask();
+        final AnimatingActivityRegistry registry = rootTask != null
+                ? rootTask.getAnimatingActivityRegistry()
+                : null;
+
+        // If we reparent, make sure to remove ourselves from the old animation registry.
+        if (mAnimatingActivityRegistry != null && mAnimatingActivityRegistry != registry) {
+            mAnimatingActivityRegistry.notifyFinished(this);
+        }
+
+        mAnimatingActivityRegistry = registry;
     }
 
     /**
--- services/core/java/com/android/server/wm/Task.java
+++ services/core/java/com/android/server/wm/Task.java
@@ -1259,6 +1259,9 @@ class Task extends TaskFragment {
         adjustBoundsForDisplayChangeIfNeeded(getDisplayContent());
 
         mRootWindowContainer.updateUIDsPresentOnDisplay();
+
+        // Ensure all animations are finished at same time in split-screen mode.
+        forAllActivities(ActivityRecord::updateAnimatingActivityRegistry);
     }
 
     @Override
