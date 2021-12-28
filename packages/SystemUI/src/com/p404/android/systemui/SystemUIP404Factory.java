671fdd7f31468b0152539883b719cee930495018
Anay Wadhera <awadhera@berkeley.edu>
2021-12-28 08:31:51 +0530

    SystemUI: integrate smartspace
    404: Smartspace jar downgraded to Nov one.
    
    Change-Id: I6cf1e5147b502a0596e9cb2cc88eaefbd9487b50
    Signed-off-by: Akash Srivastava <akashniki@gmail.com>

--- packages/SystemUI/src/com/p404/android/systemui/SystemUIP404Factory.java
+++ packages/SystemUI/src/com/p404/android/systemui/SystemUIP404Factory.java
@@ -1,12 +1,19 @@
 package com.p404.android.systemui;
 
 import android.content.Context;
+import android.content.res.AssetManager;
+import android.os.Handler;
 
 import com.p404.android.systemui.dagger.DaggerGlobalRootComponentP404;
 import com.p404.android.systemui.dagger.GlobalRootComponentP404;
+import com.p404.android.systemui.dagger.SysUIComponentP404;
 
 import com.android.systemui.SystemUIFactory;
 import com.android.systemui.dagger.GlobalRootComponent;
+import com.android.systemui.screenshot.ScreenshotNotificationSmartActionsProvider;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
 
 public class SystemUIP404Factory extends SystemUIFactory {
     @Override
@@ -14,5 +21,13 @@ public class SystemUIP404Factory extends SystemUIFactory {
         return DaggerGlobalRootComponentP404.builder()
                 .context(context)
                 .build();
+    }
+
+    @Override
+    public void init(Context context, boolean fromTest) throws ExecutionException, InterruptedException {
+        super.init(context, fromTest);
+        if (shouldInitializeComponents()) {
+            ((SysUIComponentP404) getSysUIComponent()).createKeyguardSmartspaceController();
+        }
     }
 }
