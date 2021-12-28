b8a577d850fd69df761129deabb603cba0fa65c7
Anay Wadhera <awadhera@berkeley.edu>
2021-12-28 05:49:21 +0530

    SystemUI: Initial SystemUI stub
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Change-Id: Iadebea72cc4e633c61a1c7f008a5e9baca61cd40

---
+++ packages/SystemUI/src/com/p404/android/systemui/dagger/SysUIComponentP404.java
@@ -0,0 +1,25 @@
+package com.p404.android.systemui.dagger;
+
+import com.android.systemui.dagger.DefaultComponentBinder;
+import com.android.systemui.dagger.DependencyProvider;
+import com.android.systemui.dagger.SysUISingleton;
+import com.android.systemui.dagger.SystemUIBinder;
+import com.android.systemui.dagger.SysUIComponent;
+import com.android.systemui.dagger.SystemUIModule;
+
+import dagger.Subcomponent;
+
+@SysUISingleton
+@Subcomponent(modules = {
+        DefaultComponentBinder.class,
+        DependencyProvider.class,
+        SystemUIBinder.class,
+        SystemUIModule.class,
+        SystemUIP404Module.class})
+public interface SysUIComponentP404 extends SysUIComponent {
+    @SysUISingleton
+    @Subcomponent.Builder
+    interface Builder extends SysUIComponent.Builder {
+        SysUIComponentP404 build();
+    }
+}
