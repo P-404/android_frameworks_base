5113fac7753a4bbf7d099249d7b4749f52d4a79e
Bin Huang <ot_bin.huang@mediatek.com>
2022-07-27 13:36:49 +0800

    Not run hideDotView as an animation
    When click “Video” button in camera view, the mediaRecorder will run prepare(). With the same time, screenDecoration in statusbar will run hideDotView() as an animation.
    
    It will make “mediaRecorder run prepare()” cost more time than S load.
    As in android S, the animation of hideDotView() will not run.
    So suggest not to run hideDotView() as an animation, but run function directly.
    
    Bug: 240375342
    Test:Manual
    1. Open camera app.
    2. Let the camera pattern be Picture.
    3. Click the “Video” button in camera view, let the pattern change from Picture to Video.
    
    Change-Id: I76f310fd96a1c07c1c3739c7d671b00a9301a19f
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/events/PrivacyDotViewController.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/events/PrivacyDotViewController.kt
@@ -507,7 +507,7 @@ class PrivacyDotViewController @Inject constructor(
             if (shouldShow && state.designatedCorner != null) {
                 showDotView(state.designatedCorner, true)
             } else if (!shouldShow && state.designatedCorner != null) {
-                hideDotView(state.designatedCorner, true)
+                hideDotView(state.designatedCorner, false)
             }
         }
 
