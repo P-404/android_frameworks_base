f0b317a7ebb838d9bebf8fff57fe4d3b9c983cc1
ReallySnow <reallysnow233@gmail.com>
2022-07-30 05:26:45 +0800

    SystemUI: Add Brightness slider into QQS
    Change-Id: I8a7b2b69ffa1e4705dafe0e2a982d02084aa7dc5

--- packages/SystemUI/src/com/android/systemui/qs/QSFragment.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSFragment.java
@@ -453,6 +453,7 @@ public class QSFragment extends LifecycleFragment implements QS, CommandQueue.Ca
 
     public void setBrightnessMirrorController(
             BrightnessMirrorController brightnessMirrorController) {
+        mQuickQSPanelController.setBrightnessMirror(brightnessMirrorController);
         mQSPanelController.setBrightnessMirror(brightnessMirrorController);
     }
 
