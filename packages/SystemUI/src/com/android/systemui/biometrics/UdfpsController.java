ed661f4ea3a6c15b355d8eba59e681bb601dc790
Vishal Cj <vishalcj@aospa.co>
2022-09-23 05:32:54 +0700

    udfps: Forward port Boost framework hook.
    * In my testing on OnePlus9 Pro, this had a drastic effect.
      Speeds went from 0.87s to 0.38s (average from 10 trials)
    
    * 2 second headroom is enough as the PerfLock is triggered
      showUdfpsOverlay and released onFingerUp.
    
    @markakash: Forward port to Android 13
    
    Change-Id: Icb624961f27c61ca5d55caf1e016f5fc2f19c57c

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
@@ -40,6 +40,7 @@ import android.os.Process;
 import android.os.Trace;
 import android.os.VibrationAttributes;
 import android.os.VibrationEffect;
+import android.util.BoostFramework;
 import android.util.Log;
 import android.util.RotationUtils;
 import android.view.LayoutInflater;
@@ -165,6 +166,11 @@ public class UdfpsController implements DozeReceiver {
     private boolean mOnFingerDown;
     private boolean mAttemptedToDismissKeyguard;
     private final Set<Callback> mCallbacks = new HashSet<>();
+
+    // Boostframework for UDFPS
+    private BoostFramework mPerf = null;
+    private boolean mIsPerfLockAcquired = false;
+    private static final int BOOST_DURATION_TIMEOUT = 2000;
 
     @VisibleForTesting
     public static final VibrationAttributes VIBRATION_ATTRIBUTES =
@@ -663,6 +669,8 @@ public class UdfpsController implements DozeReceiver {
 
         udfpsHapticsSimulator.setUdfpsController(this);
         udfpsShell.setUdfpsOverlayController(mUdfpsOverlayController);
+
+        mPerf = new BoostFramework();
     }
 
     /**
@@ -716,6 +724,13 @@ public class UdfpsController implements DozeReceiver {
             mOrientationListener.enable();
         } else {
             Log.v(TAG, "showUdfpsOverlay | the overlay is already showing");
+        }
+
+        if (mPerf != null && !mIsPerfLockAcquired) {
+            mPerf.perfHint(BoostFramework.VENDOR_HINT_FIRST_LAUNCH_BOOST,
+                    null,
+                    BOOST_DURATION_TIMEOUT);
+            mIsPerfLockAcquired = true;
         }
     }
 
@@ -890,6 +905,10 @@ public class UdfpsController implements DozeReceiver {
         mOnFingerDown = false;
         if (view.isIlluminationRequested()) {
             view.stopIllumination();
+        }
+       if (mPerf != null && mIsPerfLockAcquired) {
+            mPerf.perfLockRelease();
+            mIsPerfLockAcquired = false;
         }
     }
 
