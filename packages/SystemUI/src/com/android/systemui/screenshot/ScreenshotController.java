7f293a703d4c4b4af981e635baa9e7f8e5b4e882
nift4 <nift4@protonmail.com>
2023-05-11 16:14:00 +0200

    SystemUI: fix race condition in ScreenshotController
    // java.lang.IllegalStateException
    //      at android.media.MediaPlayer._start(Native Method)
    //      at android.media.MediaPlayer.startImpl(MediaPlayer.java:1371)
    //      at android.media.MediaPlayer.start(MediaPlayer.java:1345)
    //      at com.android.systemui.user.CreateUserActivity$$ExternalSyntheticLambda1.run(R8$$SyntheticClass:194)
    //      at java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1137)
    //      at java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:637)
    //      at java.lang.Thread.run(Thread.java:1012)
    //
    ** Monkey aborted due to error.
    
    Change-Id: Ic159f3c32d2074f4002dc36f8cd24cc518ba288f
    Signed-off-by: klozz <carlosj@klozz.dev>

--- packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
@@ -268,7 +268,7 @@ public class ScreenshotController {
     private final WindowManager mWindowManager;
     private final WindowManager.LayoutParams mWindowLayoutParams;
     private final AccessibilityManager mAccessibilityManager;
-    private final ListenableFuture<MediaPlayer> mCameraSound;
+    private ListenableFuture<MediaPlayer> mCameraSound;
     private final ScrollCaptureClient mScrollCaptureClient;
     private final PhoneWindow mWindow;
     private final DisplayManager mDisplayManager;
@@ -470,9 +470,12 @@ public class ScreenshotController {
         // Note that this may block if the sound is still being loaded (very unlikely) but we can't
         // reliably release in the background because the service is being destroyed.
         try {
-            MediaPlayer player = mCameraSound.get();
-            if (player != null) {
-                player.release();
+            if (mCameraSound != null) {
+                MediaPlayer player = mCameraSound.get();
+                if (player != null) {
+                    player.release();
+                }
+                mCameraSound = null;
             }
         } catch (InterruptedException | ExecutionException e) {
         }
@@ -889,15 +892,19 @@ public class ScreenshotController {
     }
 
     private void playCameraSound() {
-        mCameraSound.addListener(() -> {
-            try {
-                MediaPlayer player = mCameraSound.get();
-                if (player != null) {
-                    player.start();
+        if (mCameraSound != null) {
+            mCameraSound.addListener(() -> {
+                try {
+                    if (mCameraSound != null) {
+                        MediaPlayer player = mCameraSound.get();
+                        if (player != null) {
+                            player.start();
+                        }
+                    }
+                } catch (InterruptedException | ExecutionException e) {
                 }
-            } catch (InterruptedException | ExecutionException e) {
-            }
-        }, mBgExecutor);
+            }, mBgExecutor);
+        }
     }
 
     /**
