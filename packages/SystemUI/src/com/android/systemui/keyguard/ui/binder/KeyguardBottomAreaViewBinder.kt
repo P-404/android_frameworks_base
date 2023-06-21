8a3995d60ef8e6d63505c93a4eddbe0d5016aede
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2023-06-21 18:21:55 +0200

    SystemUI: Check if primitive vibrations are supported in new quick affordances
     * and provide an alternative if not
    
    Change-Id: I9b6fafd731c49bf4d875b0ad581ee1b4666daf17

--- packages/SystemUI/src/com/android/systemui/keyguard/ui/binder/KeyguardBottomAreaViewBinder.kt
+++ packages/SystemUI/src/com/android/systemui/keyguard/ui/binder/KeyguardBottomAreaViewBinder.kt
@@ -397,6 +397,12 @@ object KeyguardBottomAreaViewBinder {
         private val longPressDurationMs = ViewConfiguration.getLongPressTimeout().toLong()
         private var longPressAnimator: ViewPropertyAnimator? = null
 
+        private val areAllPrimitivesSupported = vibratorHelper?.areAllPrimitivesSupported(
+            VibrationEffect.Composition.PRIMITIVE_TICK,
+            VibrationEffect.Composition.PRIMITIVE_QUICK_RISE,
+            VibrationEffect.Composition.PRIMITIVE_QUICK_FALL
+        ) ?: false
+
         @SuppressLint("ClickableViewAccessibility")
         override fun onTouch(v: View?, event: MotionEvent?): Boolean {
             return when (event?.actionMasked) {
@@ -484,7 +490,12 @@ object KeyguardBottomAreaViewBinder {
                                             CycleInterpolator(ShakeAnimationCycles)
                                         shakeAnimator.start()
 
-                                        vibratorHelper?.vibrate(Vibrations.Shake)
+                                        vibratorHelper?.vibrate(
+                                            if (areAllPrimitivesSupported) {
+                                                Vibrations.Shake
+                                            } else {
+                                                Vibrations.ShakeAlt
+                                            })
                                     }
                                 } else {
                                     null
@@ -507,9 +518,17 @@ object KeyguardBottomAreaViewBinder {
             view.setOnClickListener {
                 vibratorHelper?.vibrate(
                     if (viewModel.isActivated) {
-                        Vibrations.Activated
+                        if (areAllPrimitivesSupported) {
+                            Vibrations.Activated
+                        } else {
+                            Vibrations.ActivatedAlt
+                        }
                     } else {
-                        Vibrations.Deactivated
+                        if (areAllPrimitivesSupported) {
+                            Vibrations.Deactivated
+                        } else {
+                            Vibrations.DeactivatedAlt
+                        }
                     }
                 )
                 viewModel.onClicked(
@@ -637,6 +656,7 @@ object KeyguardBottomAreaViewBinder {
                     }
                 }
                 .compose()
+        val ShakeAlt = VibrationEffect.createPredefined(VibrationEffect.EFFECT_DOUBLE_CLICK)
 
         val Activated =
             VibrationEffect.startComposition()
@@ -651,6 +671,7 @@ object KeyguardBottomAreaViewBinder {
                     0,
                 )
                 .compose()
+        val ActivatedAlt = VibrationEffect.createPredefined(VibrationEffect.EFFECT_HEAVY_CLICK)
 
         val Deactivated =
             VibrationEffect.startComposition()
@@ -665,5 +686,6 @@ object KeyguardBottomAreaViewBinder {
                     0,
                 )
                 .compose()
+        val DeactivatedAlt = VibrationEffect.createPredefined(VibrationEffect.EFFECT_HEAVY_CLICK)
     }
 }
